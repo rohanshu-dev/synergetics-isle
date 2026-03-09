f062eaabe7c1d8abdcef5011f21bc81c777d61dd
rohanshu-dev <hello.rohanshu@gmail.com>
2026-03-09 09:18:49 +0530

    favicon and title update

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Synergetics",
+    pageTitleSuffix: "by Buckminster Fuller",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
